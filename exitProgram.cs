069af8f66c32fcd9a3954b35f16057048b55fdee
tjBoatner <boatraj@dunwoody.edu>
2020-03-02 11:34:56 -0600

    formatting text to make easier to read

--- exitProgram.cs
+++ exitProgram.cs
@@ -5,7 +5,7 @@ namespace assignment_one
     class exitProgram
     {
         
-        public string exitMsg = $"Thank you for choosing MPLS Dog Boarding!\nPlease press [Enter] key to exit the program";
+        public string exitMsg = $"---Thank you for choosing MPLS Dog Boarding!---\n---Please press [Enter] key to exit the program---";
         public string goodbyeUser()
         {
             return String.Format(exitMsg);
